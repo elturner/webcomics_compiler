7fe52bdde75a6a59f85c3e60e42409846976894a
Eric Turner <elturner@eecs.berkeley.edu>
2015-07-19 07:43:43 -0700

    removed push to remote server.  instead page is opened locally.

--- driver.sh
+++ driver.sh
@@ -5,10 +5,7 @@ cd `dirname $0`
 echo "preparing..."
 ./wcc.sh
 
-echo "passing to server..."
-sshpass scp comics.html eecs:~/public_html/
-
-echo "opening page in firefox..."
-firefox http://www.eecs.berkeley.edu/~elturner/comics.html &
+echo "opening page..."
+open comics.html &
 
 echo "done"
