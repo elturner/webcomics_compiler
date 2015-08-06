136121fab7629c0802815eef5cbebe2c34089a30
Eric Turner <elturner@eecs.berkeley.edu>
2015-08-06 06:19:27 -0700

    fixed bugs in comic parsing

--- dilbert.sh
+++ dilbert.sh
@@ -15,6 +15,7 @@ echo "<h2><a href="http://www.dilbert.com/">Dilbert</a></h2>"
 echo "<br>"
 echo "<br>"
 cat ${dilbert_file} | sed -n "s/.*<img alt=\"\(.*\)\" class=\".*\" height=\".*\" src=\"\(.*\)\" width=\".*\" \/>/<img src=\"\2\"><\/img><br>\1<br><br>/p"
+cat ${dilbert_file} | sed -n "s/.*<img class=\".*\" width=\".*\" height=\".*\" alt=\"\(.*\)\" src=\"\(.*\)\" \/>/<img src=\"\2\"><\/img><br>\1<br><br>/p"
 echo "<br>"
 echo "<br>"
 
