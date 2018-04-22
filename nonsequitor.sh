32692d823cf9ddac48e900938a81935dcc6083bd
Eric Turner <elturner@eecs.berkeley.edu>
2018-04-22 07:01:07 -0700

    fix to nonsequitur and to smbc

--- nonsequitor.sh
+++ nonsequitor.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-nonsequitor_file="nonsequitor.html"
+nonsequitor_file="nonsequitur.html"
 
 # This script will download the latest nonsequitor, and
 # output a formatted version of the comic's image
 # in html to the screen
 
+# filename based on today's date
+today=`date +%Y/%m/%d`
+
 # get latest file from website
-wget http://www.gocomics.com/nonsequitur/ -o /dev/null -O ${nonsequitor_file}
+wget http://www.gocomics.com/nonsequitur/${today} -o /dev/null -O ${nonsequitor_file}
 
 # label as Non Sequitor
 echo "<hr>"
-echo "<h2><a href=\"http://www.gocomics.com/nonsequitur/\">Non Sequitor</a></h2>"
+echo "<h2><a href=\"http://www.gocomics.com/nonsequitur/\">Non Sequitur</a></h2>"
 echo "<br>"
 
 # get information
-cat ${nonsequitor_file} | sed -n "s/<picture class=.*><img width=.* sizes=.* srcset=.* src=\"\(.*\)\" \/><\/picture>/<br><img src=\"\1\"><\/img><br><br>/p"
+cat ${nonsequitor_file} | sed -n "s/data-image=\"\(.*\)\"/<br><img src=\"\1\"><\/img><br><br>/p"
 
 # clean up
 rm -f ${nonsequitor_file}
